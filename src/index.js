075b2c76129eac8b83d104a5b9aeb15688cd91c5
Andrey Shulmin <networkdaemonyung@gmail.com>
2020-08-15 16:35:40 +0300

    fucking bullshit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.scss';
+
+class Game {
+  gameId = Date.now();
+
+  log() {
+    // eslint-disable-next-line no-console
+    console.log(this.gameId);
+  }
+}
+
+const game = new Game();
+game.log();
