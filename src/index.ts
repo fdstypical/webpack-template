c605abf92a887e869b4123ad3cdb65445aa2baac
Andrey Shulmin <networkdaemonyung@gmail.com>
2020-08-22 21:54:07 +0300

    fix .eslintrc for typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import "@assets/scss/index.scss";
-import "@src/style.scss";
+import '@assets/scss/index.scss';
+import '@src/style.scss';
 
-import calc from "@src/script";
+import calc from '@src/script';
 
 class Game {
   gameId: number = Date.now();
