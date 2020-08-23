4c815b77fd8a032cd751b06ba2a3d1c9d0f8ddc2
Andrey Shulmin <networkdaemonyung@gmail.com>
2020-08-23 23:45:09 +0300

    remove script.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
 import '@assets/scss/index.scss';
 import '@src/style.scss';
 
-import calc from '@src/script';
-
 class Game {
   gameId: number = Date.now();
-  calc: number = calc(10, 25);
 
   log() {
     // eslint-disable-next-line no-console
--- src/script.ts
+++
@@ -1,7 +0,0 @@
-const calc = (num1: number, num2: number): number => num1 + num2;
-
-const b = calc(1, 10);
-// eslint-disable-next-line no-console
-console.log(b);
-
-export default calc;
