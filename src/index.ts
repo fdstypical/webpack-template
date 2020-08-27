24bd17184f22e8a7b91a6e6d95520c5a662b8695
Andrey Shulmin <networkdaemonyung@gmail.com>
2020-08-27 20:36:54 +0300

    remove useless code from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
-import '@assets/scss/index.scss';
-import '@src/style.scss';
-
-class Game {
-  gameId: number = Date.now();
-
-  log() {
-    // eslint-disable-next-line no-console
-    console.log(this.gameId);
-  }
-}
-
-const game = new Game();
-game.log();
+import "@assets/scss/index.scss";
